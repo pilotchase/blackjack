422ed4c7ee04cf42811b8f84a13d6994c8e3dee0
Chase Morgan <pilotchase@users.noreply.github.com>
2018-05-17 19:48:53 -0700

    Create BlackJackDriver.java

---
+++ BlackJackDriver.java
@@ -0,0 +1,14 @@
+
+/**
+(c) C. Morgan - 2018
+*/
+
+public class BlackJackDriver
+{
+    public static void main(String[] args)
+    {
+        //create new game
+        Game g = new Game();
+        g.play();
+    }
+}
