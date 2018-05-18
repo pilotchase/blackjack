7a36a3a6abf4773de9b483cf35fb47250dd21250
Chase Morgan <pilotchase@users.noreply.github.com>
2018-05-17 19:49:37 -0700

    Update BlackJackDriver.java

--- BlackJackDriver.java
+++ BlackJackDriver.java
@@ -8,7 +8,7 @@ public class BlackJackDriver
     public static void main(String[] args)
     {
         //create new game
-        Game g = new Game();
-        g.play();
+        Game myGame = new Game();
+        myGame.play();
     }
 }
